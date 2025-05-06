3820fd1c8e36be3ef21b9a8ef002ed9bd1ecce94
Neeraj Pratap Hazarika <neerajprataphazarika@gmail.com>
2025-05-06 12:55:07 +0200

    bot v2, fixed opponent win prevention

--- src/main/java/ch/qc/starter/AlexikoBot.java
+++ src/main/java/ch/qc/starter/AlexikoBot.java
@@ -2,79 +2,77 @@ package ch.qc.starter;
 
 import ch.cern.quantumconnect.core.Algorithm;
 import ch.cern.quantumconnect.core.QuantumField;
-// import ch.cern.quantumconnect.core.SpaceOwnership;
 
-public class AlexikoBot implements Algorithm  {
-
+public class AlexikoBot implements Algorithm {
 
     @Override
     public int accelerateQuark(QuantumField quantumField) {
-      int columns = quantumField.getNumberOfColumns();
-      int[] validColumns = new int[columns];
-      int validCount = 0;
-      int myRow, opponentRow = 0;
-  
-      
-      for (int col = 0; col < columns; col++) {
-        // Collect all non-full columns
-        if (!quantumField.isColumnFull(col)) {
-          validColumns[validCount++] = col;
-        }
-      }
+        int columns = quantumField.getNumberOfColumns();
+        int rows = quantumField.getNumberOfRows(); // Get the number of rows
+        int[] validColumns = new int[columns];
+        int validCount = 0;
 
-      // iterate all valid columns for preventing opponent win
-        for (int i = 0; i < validCount; i++) {
-            int col = validColumns[i];
-
-            // reinitialize opponentRow for each column
-            opponentRow = 0;
-
-            // iterate all rows in the column to check if opponent has win in next move
-            for (int row = 0; row < quantumField.getNumberOfRows(); row++) {
-                // check if column has atleast 5 opponentRows 
-                if ((!quantumField.isSpaceEmpty(row, col) && !quantumField.belongsToMe(row, col)) || quantumField.isSpaceEmpty(row, col)) {
-                    opponentRow += 1;
-                }
-                else {
-                    opponentRow -= 1;
-                }
-
-                // check if column has atleast 5 rows for opponent and column not full
-                if (opponentRow >= 5 && !quantumField.isColumnFull(col)) {
-                    return col;
-                }
+        for (int col = 0; col < columns; col++) {
+            // Collect all non-full columns
+            if (!quantumField.isColumnFull(col)) {
+                validColumns[validCount++] = col;
             }
         }
 
-        // iterate all valid columns for our win
+        // Check for opponent win
         for (int i = 0; i < validCount; i++) {
             int col = validColumns[i];
+            int opponentRow = 0;
 
-            // reinitialize myRow for each column
-            myRow = 0;
+            for (int row = 0; row < rows; row++) {
+                if (!quantumField.isSpaceEmpty(row, col) && !quantumField.belongsToMe(row, col)) {
+                    opponentRow++;
+                } else if (quantumField.belongsToMe(row, col)) {
+                    opponentRow--;
+                }
 
-            // if opponent has not win in next move, check if column has available 6 rows empty
-            for (int row = 0; row < quantumField.getNumberOfRows(); row++) {
-                // Check if column has atleast 6 continous myRow 
+                if (opponentRow >= 5) {
+                    return col;
+                }
+                }
+        }
+
+        // Check for our win
+        for (int i = 0; i < validCount; i++) {
+            int col = validColumns[i];
+            int myRow = 0;
+
+            for (int row = 0; row < rows; row++) {
                 if (quantumField.isSpaceEmpty(row, col) || quantumField.belongsToMe(row, col)) {
-                    myRow += 1;
-                }
-                else {
-                    myRow -= 1;
-                } 
-
-                // if column has 6 empty rows, return the column
-                if (myRow >= 6) {
-                    return col;
+                    myRow = checkVertical(quantumField, row, col, 6); // Check for 6 in a row
+                    if (myRow >= 6) {
+                        return col;
+                    }
                 }
             }
         }
-  
-      // Pick a random column from the valid ones
-      if (validCount > 0) {
-        return validColumns[0];
-      }
-  
-      throw new IllegalStateException("No valid moves available");
+
+        // Pick a random column from the valid ones
+        if (validCount > 0) { 
+            return validColumns[0];
+        }
+
+        throw new IllegalStateException("No valid moves available");
+    }
+
+    private int checkVertical(QuantumField quantumField, int row, int col, int targetCount) {
+        int count = 0;
+        int rows = quantumField.getNumberOfRows();
+
+        for (int i = row; i < rows; i++) {
+            if (quantumField.isSpaceEmpty(i, col)) {
+                count++;
+            } else if (quantumField.belongsToMe(i, col)) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return count;
     }
 }
--- src/main/java/ch/qc/starter/EmulatorRunner.java
+++ src/main/java/ch/qc/starter/EmulatorRunner.java
@@ -5,7 +5,7 @@ import ch.cern.quantumconnect.core.Experiment;
 public class EmulatorRunner {
 
   public static void main(String[] args) throws InterruptedException {
-    var exp = new Experiment(new AlexikoBot(), new RandomBot());
+    var exp = new Experiment(new AlexikoBot(), new AlexikoBot());
     exp.runExperiment();
   }
 }
